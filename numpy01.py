96c7b66eb9ce74cc9f17daa468a632b26d31434c
Konrad Hafen <konrad.hafen@gmail.com>
2018-03-13 15:37:35 -0700

    first commit

---
+++ numpy01.py
@@ -0,0 +1,38 @@
+# import numpy package
+import numpy as np
+
+#create and print array containing a single value of 1
+a = np.array(1)
+print a
+
+#create and print a 1D array
+a = np.array([1,2])
+print a
+
+#create and print a 2D array
+a = np.array([
+[1,2,3],
+[1,2,3],
+[1,2,3]
+])
+print a
+
+#create and print a 3D array
+a = np.array([
+[
+[1,2,3],
+[1,2,3],
+[1,2,3]
+],
+[
+[1,2,3],
+[1,2,3],
+[1,2,3]
+],
+[
+[1,2,3],
+[1,2,3],
+[1,2,3]
+]
+])
+print a

c967f877892c59bd116988cf1500b6997dca02c8
Konrad Hafen <33733846+konradhafen@users.noreply.github.com>
2018-03-17 16:59:42 -0700

    Update numpy01.py

--- numpy01.py
+++ numpy01.py
@@ -6,19 +6,19 @@ a = np.array(1)
 print a
 
 #create and print a 1D array
-a = np.array([1,2])
-print a
+a1d = np.array([1,2])
+print a1d
 
 #create and print a 2D array
-a = np.array([
+a2d = np.array([
 [1,2,3],
 [1,2,3],
 [1,2,3]
 ])
-print a
+print a2d
 
 #create and print a 3D array
-a = np.array([
+a3d = np.array([
 [
 [1,2,3],
 [1,2,3],
@@ -35,4 +35,4 @@ a = np.array([
 [1,2,3]
 ]
 ])
-print a
+print a3d

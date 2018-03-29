f3784911fb2f0bb3e295a0da754a969d695d56ae
Konrad Hafen <konrad.hafen@gmail.com>
2018-03-29 11:14:10 -0700

    tutorial content

--- numpy005_MethodsToCreateArrays.py
+++ numpy005_MethodsToCreateArrays.py
@@ -1,2 +1,10 @@
 #import the numpy package
 import numpy as np
+
+#create an 'empty' array
+a = np.empty((2,2), dtype=np.float32)
+print a
+
+#now an array of ints
+b = np.empty((5,3), dtype=np.int)
+print b

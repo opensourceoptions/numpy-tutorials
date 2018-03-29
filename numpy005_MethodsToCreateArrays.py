41912c9c7b4f24949e6a0a938c9ffb7603ba998b
Konrad Hafen <konrad.hafen@gmail.com>
2018-03-29 16:46:29 -0700

    tutorial code

--- numpy005_MethodsToCreateArrays.py
+++ numpy005_MethodsToCreateArrays.py
@@ -24,3 +24,24 @@ print z
 #create an array of ones
 o = np.ones((3,4), dtype=np.int)
 print o
+
+#create an array with 2 sequential elements
+a = np.arange(2)
+print a
+
+#create an array with the values 5-10
+a = np.arange(5,11)
+print a
+
+#create an array with even values from 10-30
+a = np.arange(10,31,2)
+print a
+
+#create a 3x3x3 array ranging from 0 to 26
+a = np.arange(27).reshape((3,3,3))
+print a
+
+#create array from inputdata.csv
+a = np.genfromtxt("importdata.csv", dtype=np.float32, delimiter=",")
+print a
+

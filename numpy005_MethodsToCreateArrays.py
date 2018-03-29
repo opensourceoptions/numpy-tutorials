0f32c85ee8f1f2421ca092353e0c4df9b3b26339
Konrad Hafen <konrad.hafen@gmail.com>
2018-03-29 15:56:35 -0700

    tutorial code

--- numpy005_MethodsToCreateArrays.py
+++ numpy005_MethodsToCreateArrays.py
@@ -8,3 +8,19 @@ print a
 #now an array of ints
 b = np.empty((5,3), dtype=np.int)
 print b
+
+#fill a with the value 99.99
+a.fill(99.99)
+print a
+
+#fill b with the value 99.99
+b.fill(99.99)
+print b
+
+#create an array of zeros
+z = np.zeros((3,4), dtype=np.float32)
+print z
+
+#create an array of ones
+o = np.ones((3,4), dtype=np.int)
+print o
